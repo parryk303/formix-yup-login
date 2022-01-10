67eb1e53e3783edddd74824dee96032e3e8a64d6
Kyle Parry <kyle.parry.303@gmail.com>
2022-01-09 19:29:45 -0700

    Add style to Success page

--- pages/components/Input.js
+++ pages/components/Input.js
@@ -2,19 +2,19 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import { ErrorMessage, useField } from 'formik';
 
-const Input = ({ label, ...props }) => {
-  const [field] = useField(props);
+const Input = ({ label, name, ...props }) => {
+  const [field] = useField(name);
   return (
     <div className='mb-2'>
       <TextField
         {...field} {...props}
         margin='normal'
-        name={field.name}
+        name={name}
         label={label}
         fullWidth
         autoComplete='off'
       />
-      <ErrorMessage component='div' name={field.name} className='error' />
+      <ErrorMessage component='div' name={name} className='error' />
     </div>
   )
 }
